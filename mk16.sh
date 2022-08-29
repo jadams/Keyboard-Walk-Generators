371cea88d9dabe30e6a50b3b062c17bb4b751377
John Adams <git@jada.ms>
2022-08-29 16:36:34 -0400

    Update mk16.sh
    lowercase

--- mk16.sh
+++ mk16.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-set -euo pipefail
-cd Method\ 2\ -\ Combinator\ Script/
-python3 Combinator.py 4_Walk_seed.txt -l 8 > 8_Walk.txt
-python3 Combinator.py 8_Walk.txt -l 16 > 16_Walk.txt
-mv 16_Walk.txt ../16_Walk.txt
-rm 8_Walk.txt
+set -xeuo pipefail
+cd "Method 2 - Combinator Script"
+python3 Combinator.py 4_walk_seed.txt -l 8 > 8_walk.txt
+python3 Combinator.py 8_walk.txt -l 16 > 16_walk.txt
+mv 16_walk.txt ../16_walk.txt
+rm 8_walk.txt
 cd -
 
